7fedd1a2e41032830cd7f22b4f625b955112e991
BELLO ABDULMATEEN <abdulmateenbinbello@gmail.com>
2020-04-11 12:21:51 +0100

    Challenge one completed

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,22 @@
-const covid19ImpactEstimator = (data) => data;
-
+/* eslint-disable prefer-const */
+/* eslint-disable linebreak-style */
+/* eslint-disable no-unused-vars */
+let outputData = {
+  data: {
+  },
+  impact: {
+    currentlyInfected: 0,
+    infectionsByRequestedTime: 0
+  },
+  severImpact: {
+    currentlyInfected: 0,
+    infectionsByRequestedTime: 0
+  }
+};
+const covid19ImpactEstimator = (data) => {
+  outputData.impact.currentlyInfected = data.reportedCases * 10;
+  outputData.severImpact.currentlyInfected = data.reportedCases * 50;
+  outputData.impact.infectionsByRequestedTime = outputData.impact.currentlyInfected * 512;
+  outputData.severImpact.infectionsByRequestedTime = outputData.severImpact.currentlyInfected * 512;
+};
 export default covid19ImpactEstimator;
